94881654ba20b00fdf32822ff670625ea542ffee
sanputec <tec@snpapp.com>
2025-04-16 15:40:31 +0800

    app.py

--- app.py
+++ app.py
@@ -68,3 +68,9 @@ def slack_commands():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
+@app.route("/healthz", methods=["GET"])
+def health_check():
+    return "OK", 200
+
